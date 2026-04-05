a9cf5be802c1f523fd9829703b73fa38cb66cf53
Kohei Tanaka <kohei_1020@icloud.com>
2026-04-06 00:22:54 +0900

    Initial Commit

---
+++ KCM App/KCM_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCM_AppApp.swift
+//  KCM App
+//
+//  Created by 田中昂平 on 2026/04/06.
+//
+
+import SwiftUI
+
+@main
+struct KCM_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
